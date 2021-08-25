c31554940b402dd6f55156a5444208103706c0fb
Shreyansh Tiwari <60467135+coziamshreyansh@users.noreply.github.com>
2021-08-25 22:46:11 +0530

    Create Count Subsets with Given Sum.cpp

---
+++ DP/Count Subsets with Given Sum.cpp
@@ -0,0 +1,32 @@
+int perfectSum(int arr[], int n, int sum)
+	{
+         int dp[n+1][sum+1];
+        // for(int i = 0; i <= N; i++){
+        //     dp[i] = new bool[sum+1];
+        // }
+        
+        
+        // handling base cases 
+        for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= sum; j++) {
+			if (i == 0)
+				dp[i][j] = 0;
+			if (j == 0)
+				dp[i][j] = 1;
+		}
+     	}
+        //handling choices 
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=sum; j++){
+                if(j >= arr[i-1]){
+                    dp[i][j]= ((dp[i-1][j])%1000000007 + (dp[i-1][j-arr[i-1]])%1000000007)%1000000007;
+                }
+                else {
+                    dp[i][j]= dp[i-1][j]%1000000007;
+                }
+            }
+        }
+        return dp[n][sum];
+        
+	
+	}
