109bb3e34eea5fda9a57317d27d4b1f8d73dbfb2
Shreyansh Tiwari <60467135+coziamshreyansh@users.noreply.github.com>
2021-09-09 23:01:00 +0530

    WEEK 1

---
+++ ARRAY/238. Product of Array Except Self.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> res(size);
+        res[0] = 1;
+        for(int i=1; i<size;i++){
+            res[i] = res[i-1]*nums[i-1];
+        }
+        int right = 1;
+        for(int i= size-1; i>=0; i--){
+            
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+};
