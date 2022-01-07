6c3d8767c645705ad0ae62fd13e8578e5ad4c86c
Shreyansh Tiwari <60467135+coziamshreyansh@users.noreply.github.com>
2022-01-07 23:49:14 +0530

    Create Climbing Stairs.cpp

---
+++ DP LEVEL WISE/Climbing Stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        unordered_map<int , int> memo;
+        return total(0, n, memo);
+    }
+    int total(int cs, int t, unordered_map<int, int> &memo)
+    {
+        if( cs == t)
+            return 1;
+        if(cs > t)
+            return 0;
+        if(memo.find(cs) != memo.end())
+            return memo[cs];
+        int ways = total(cs+1, t, memo) + total(cs+2, t, memo);
+        memo[cs] = ways;
+        return ways;
+    }
+};
